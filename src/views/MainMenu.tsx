42c71753f29f6a559990959126e5f93975332b1a
Jason A. Wise <jasonandrewwise@gmail.com>
2022-08-19 21:35:40 -0400

    updating

--- src/views/MainMenu.tsx
+++ src/views/MainMenu.tsx
@@ -3,14 +3,14 @@ import * as React from 'react'
 export default function MainMenu() {
   const [menuChoice, setMenuChoice] = React.useState<number>(0)
 
-  function handleOnClick({ event: { target: id } }: any): void {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    setMenuChoice(id)
+  function handleOnClick(e: any): void {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+    setMenuChoice(e.currentTarget.id)
   }
 
   return (
     <div>
-      <h1>Main Menu</h1>
+      <h1 className="text-lg font-bold">Main Menu</h1>
       <button type="button" id="1" onClick={handleOnClick}>
         1. Create Character
       </button>
